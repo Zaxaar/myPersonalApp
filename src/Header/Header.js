d368c33e26bbc71cb6569bf9a4ee7ee9c50c8565
Zaxaar <zahar1982@gmail.com>
2016-12-08 16:10:57 +0100

    add basic navigation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import {Grid, Row, Col} from 'react-bootstrap';
 
 import logo from '../images/logo.svg';
@@ -8,8 +9,19 @@ class Header extends React.Component {
     render(){
         return (
             <Grid className="App-header" fluid>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Hello</h2>
+                <Row>
+                    <Col xs={12} sm={4} lg={4}>
+                        <h2><img src={logo} className="App-logo" alt="logo" />Hello</h2>
+                    </Col>
+
+                    <Col xs={12} sm={8} lg={8}>
+                        <ul id="App-main-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">Projekty</Link></li>
+                            <li><Link to="/">O mnie</Link></li>
+                        </ul>
+                    </Col>
+                </Row>
             </Grid>
         )
     }
